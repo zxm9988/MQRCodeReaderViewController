e58d0813b78a647c73e77e2144247a8013cb78bf
zxm99888 <zxm99888@gmail.com>
2016-05-25 12:10:25 +0800

    修改关闭摄像头权限时引起的crash

--- QRCodeReaderViewController/QRCodeReader.m
+++ QRCodeReaderViewController/QRCodeReader.m
@@ -27,22 +27,21 @@
 #import "QRCodeReader.h"
 
 @interface QRCodeReader () <AVCaptureMetadataOutputObjectsDelegate>
-@property (strong, nonatomic) AVCaptureDevice            *defaultDevice;
-@property (strong, nonatomic) AVCaptureDeviceInput       *defaultDeviceInput;
-@property (strong, nonatomic) AVCaptureDevice            *frontDevice;
-@property (strong, nonatomic) AVCaptureDeviceInput       *frontDeviceInput;
-@property (strong, nonatomic) AVCaptureMetadataOutput    *metadataOutput;
-@property (strong, nonatomic) AVCaptureSession           *session;
-@property (strong, nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
+@property(strong, nonatomic) AVCaptureDevice *defaultDevice;
+@property(strong, nonatomic) AVCaptureDeviceInput *defaultDeviceInput;
+@property(strong, nonatomic) AVCaptureDevice *frontDevice;
+@property(strong, nonatomic) AVCaptureDeviceInput *frontDeviceInput;
+@property(strong, nonatomic) AVCaptureMetadataOutput *metadataOutput;
+@property(strong, nonatomic) AVCaptureSession *session;
+@property(strong, nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
 
-@property (copy, nonatomic) void (^completionBlock) (NSString *);
+@property(copy, nonatomic) void (^completionBlock)(NSString *);
 
 @end
 
 @implementation QRCodeReader
 
-- (id)initWithMetadataObjectTypes:(NSArray *)metadataObjectTypes
-{
+- (id)initWithMetadataObjectTypes:(NSArray *)metadataObjectTypes {
   if ((self = [super init])) {
     _metadataObjectTypes = metadataObjectTypes;
 
@@ -52,79 +51,90 @@
   return self;
 }
 
-+ (instancetype)readerWithMetadataObjectTypes:(NSArray *)metadataObjectTypes
-{
++ (instancetype)readerWithMetadataObjectTypes:(NSArray *)metadataObjectTypes {
   return [[self alloc] initWithMetadataObjectTypes:metadataObjectTypes];
 }
 
 #pragma mark - Initializing the AV Components
 
-- (void)setupAVComponents
-{
-  self.defaultDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+- (void)setupAVComponents {
+  self.defaultDevice =
+      [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
 
   if (_defaultDevice) {
-    self.defaultDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:_defaultDevice error:nil];
-    self.metadataOutput     = [[AVCaptureMetadataOutput alloc] init];
-    self.session            = [[AVCaptureSession alloc] init];
-    self.previewLayer       = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    self.defaultDeviceInput =
+        [AVCaptureDeviceInput deviceInputWithDevice:_defaultDevice error:nil];
+    self.metadataOutput = [[AVCaptureMetadataOutput alloc] init];
+    self.session = [[AVCaptureSession alloc] init];
+    self.previewLayer =
+        [AVCaptureVideoPreviewLayer layerWithSession:self.session];
 
-    for (AVCaptureDevice *device in [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo]) {
+    for (AVCaptureDevice *device in
+         [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo]) {
       if (device.position == AVCaptureDevicePositionFront) {
         self.frontDevice = device;
       }
     }
 
     if (_frontDevice) {
-      self.frontDeviceInput = [AVCaptureDeviceInput deviceInputWithDevice:_frontDevice error:nil];
+      self.frontDeviceInput =
+          [AVCaptureDeviceInput deviceInputWithDevice:_frontDevice error:nil];
     }
   }
 }
 
-- (void)configureDefaultComponents
-{
+- (void)configureDefaultComponents {
   [_session addOutput:_metadataOutput];
 
   if (_defaultDeviceInput) {
     [_session addInput:_defaultDeviceInput];
   }
 
-  [_metadataOutput setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-  [_metadataOutput setMetadataObjectTypes:_metadataObjectTypes];
+  [_metadataOutput setMetadataObjectsDelegate:self
+                                        queue:dispatch_get_main_queue()];
+  NSSet *avbSet =
+      [NSSet setWithArray:_metadataOutput.availableMetadataObjectTypes];
+  NSSet *userSet = [NSSet setWithArray:_metadataObjectTypes];
+  if ([avbSet intersectsSet:userSet]) {
+    [_metadataOutput setMetadataObjectTypes:_metadataObjectTypes];
+  } else {
+    [_metadataOutput
+        setMetadataObjectTypes:_metadataOutput.availableMetadataObjectTypes];
+  }
+
   [_previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
 }
 
-- (void)switchDeviceInput
-{
+- (void)switchDeviceInput {
   if (_frontDeviceInput) {
     [_session beginConfiguration];
 
     AVCaptureDeviceInput *currentInput = [_session.inputs firstObject];
     [_session removeInput:currentInput];
 
-    AVCaptureDeviceInput *newDeviceInput = (currentInput.device.position == AVCaptureDevicePositionFront) ? _defaultDeviceInput : _frontDeviceInput;
+    AVCaptureDeviceInput *newDeviceInput =
+        (currentInput.device.position == AVCaptureDevicePositionFront)
+            ? _defaultDeviceInput
+            : _frontDeviceInput;
     [_session addInput:newDeviceInput];
 
     [_session commitConfiguration];
   }
 }
 
-- (BOOL)hasFrontDevice
-{
+- (BOOL)hasFrontDevice {
   return _frontDevice != nil;
 }
 
 #pragma mark - Controlling Reader
 
-- (void)startScanning
-{
+- (void)startScanning {
   if (![self.session isRunning]) {
     [self.session startRunning];
   }
 }
 
-- (void)stopScanning
-{
+- (void)stopScanning {
   if ([self.session isRunning]) {
     [self.session stopRunning];
   }
@@ -136,33 +146,34 @@
 
 #pragma mark - Managing the Orientation
 
-+ (AVCaptureVideoOrientation)videoOrientationFromInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
++ (AVCaptureVideoOrientation)videoOrientationFromInterfaceOrientation:
+        (UIInterfaceOrientation)interfaceOrientation {
   switch (interfaceOrientation) {
-    case UIInterfaceOrientationLandscapeLeft:
-      return AVCaptureVideoOrientationLandscapeLeft;
-    case UIInterfaceOrientationLandscapeRight:
-      return AVCaptureVideoOrientationLandscapeRight;
-    case UIInterfaceOrientationPortrait:
-      return AVCaptureVideoOrientationPortrait;
-    default:
-      return AVCaptureVideoOrientationPortraitUpsideDown;
+  case UIInterfaceOrientationLandscapeLeft:
+    return AVCaptureVideoOrientationLandscapeLeft;
+  case UIInterfaceOrientationLandscapeRight:
+    return AVCaptureVideoOrientationLandscapeRight;
+  case UIInterfaceOrientationPortrait:
+    return AVCaptureVideoOrientationPortrait;
+  default:
+    return AVCaptureVideoOrientationPortraitUpsideDown;
   }
 }
 
 #pragma mark - Checking the Reader Availabilities
 
-+ (BOOL)isAvailable
-{
++ (BOOL)isAvailable {
   @autoreleasepool {
-    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDevice *captureDevice =
+        [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
 
     if (!captureDevice) {
       return NO;
     }
 
     NSError *error;
-    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    AVCaptureDeviceInput *deviceInput =
+        [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
 
     if (!deviceInput || error) {
       return NO;
@@ -172,29 +183,31 @@
   }
 }
 
-+ (BOOL)supportsMetadataObjectTypes:(NSArray *)metadataObjectTypes
-{
++ (BOOL)supportsMetadataObjectTypes:(NSArray *)metadataObjectTypes {
   if (![self isAvailable]) {
     return NO;
   }
 
   @autoreleasepool {
     // Setup components
-    AVCaptureDevice *captureDevice    = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-    AVCaptureDeviceInput *deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
-    AVCaptureMetadataOutput *output   = [[AVCaptureMetadataOutput alloc] init];
-    AVCaptureSession *session         = [[AVCaptureSession alloc] init];
+    AVCaptureDevice *captureDevice =
+        [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDeviceInput *deviceInput =
+        [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
+    AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc] init];
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
 
     [session addInput:deviceInput];
     [session addOutput:output];
 
     if (metadataObjectTypes == nil || metadataObjectTypes.count == 0) {
       // Check the QRCode metadata object type by default
-      metadataObjectTypes = @[AVMetadataObjectTypeQRCode];
+      metadataObjectTypes = @[ AVMetadataObjectTypeQRCode ];
     }
 
     for (NSString *metadataObjectType in metadataObjectTypes) {
-      if (![output.availableMetadataObjectTypes containsObject:metadataObjectType]) {
+      if (![output.availableMetadataObjectTypes
+              containsObject:metadataObjectType]) {
         return NO;
       }
     }
@@ -205,19 +218,21 @@
 
 #pragma mark - Managing the Block
 
-- (void)setCompletionWithBlock:(void (^) (NSString *resultAsString))completionBlock
-{
+- (void)setCompletionWithBlock:
+        (void (^)(NSString *resultAsString))completionBlock {
   self.completionBlock = completionBlock;
 }
 
 #pragma mark - AVCaptureMetadataOutputObjects Delegate Methods
 
-- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
-{
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+    didOutputMetadataObjects:(NSArray *)metadataObjects
+              fromConnection:(AVCaptureConnection *)connection {
   for (AVMetadataObject *current in metadataObjects) {
-    if ([current isKindOfClass:[AVMetadataMachineReadableCodeObject class]]
-        && [_metadataObjectTypes containsObject:current.type]) {
-      NSString *scannedResult = [(AVMetadataMachineReadableCodeObject *) current stringValue];
+    if ([current isKindOfClass:[AVMetadataMachineReadableCodeObject class]] &&
+        [_metadataObjectTypes containsObject:current.type]) {
+      NSString *scannedResult =
+          [(AVMetadataMachineReadableCodeObject *)current stringValue];
 
       if (_completionBlock) {
         _completionBlock(scannedResult);
